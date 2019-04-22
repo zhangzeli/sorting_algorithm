63d582ef7a47fe75a1a515aa17cf7d3989f2dd09
zhangzeli <853089986@qq.com>
2019-04-22 11:41:20 +0800

    选择排序

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ![常见的排序算法列表](https://github.com/zhangzeli/sorting_algorithm/blob/master/src/main/resources/sorts.jpg)
 
 ## 选择排序
+>[SelectionSort](https://github.com/zhangzeli/sorting_algorithm/tree/master/src/main/java/com/zzl/SelectionSort.java)
 
 
 ## 冒泡排序
---
+++ src/main/java/com/zzl/SelectionSort.java
@@ -0,0 +1,40 @@
+package com.zzl;
+
+/**
+ * @ClassName SelectionSort
+ * @Description 选择排序
+ * @Author BossZ
+ * @Version 1.0
+ * @Date 2019/4/22-10:57
+ **/
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 6, 8, 1, 7, 9, 4, 2};
+
+        int[] sort = sort(arr);
+
+        SortUtil.print(arr);
+    }
+
+
+    public static int [] sort (int [] arr){
+        int minPos;
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            minPos = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minPos]) minPos = j;
+            }
+
+            SortUtil.swap(arr,i,minPos);
+
+        }
+
+        SortUtil.print(arr);
+        
+        return  arr;
+    }
+}
