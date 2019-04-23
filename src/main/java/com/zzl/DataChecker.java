11dd15de52e4daa8c0e9035d6997010c8e9cfd5b
zhangzeli <853089986@qq.com>
2019-04-23 10:40:51 +0800

    对数器

--- src/main/java/com/zzl/DataChecker.java
+++ src/main/java/com/zzl/DataChecker.java
@@ -35,7 +35,8 @@ public class DataChecker {
 
 //        SelectionSort.sort(arr2);
 //        BubbleSort.sort(arr2);
-        InsertionSort.sort(arr2);
+//        InsertionSort.sort(arr2);
+        ShellSort.sort(arr2);
         boolean same = true;
         for(int i=0;i<arr2.length;i++){
             if (arr[i]!=arr2[i]) same =false;
